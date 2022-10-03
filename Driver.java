3c457ae5245719c5fe541a6e6ce3e40bd0fd4b88
Tamam Makki <tamammakki@Tamams-MacBook-Pro.local>
2022-10-03 00:55:36 -0400

    modified files

--- Driver.java
+++ Driver.java
@@ -1,16 +1,30 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Driver { 
-	public static void main(String [] args)	{ 
+	public static void main(String [] args) throws IOException	{ 
 		Polynomial p = new Polynomial(); 
 		System.out.println(p.evaluate(3)); 
-		double [] c1 = {6,0,0,5}; 
-		Polynomial p1 = new Polynomial(c1); 
-		double [] c2 = {0,-2,0,0,-9}; 
-		Polynomial p2 = new Polynomial(c2);
+		double [] c1 = {2,1};
+		int [] d1 = {0,1};
+		Polynomial p1 = new Polynomial(c1, d1); 
+		double [] c2 = {4,1}; 
+		int [] d2 = {0, 1};
+		Polynomial p2 = new Polynomial(c2, d2);
 		Polynomial s = p1.add(p2); 
 		System.out.println("s(0.1) = " + s.evaluate(0.1)); 
 		if(s.hasRoot(1)) 
 			System.out.println("1 is a root of s"); 
 		else 
 			System.out.println("1 is not a root of s");
+		Polynomial multi = p1.multiply(p2);
+		System.out.println("multi(2) = " + multi.evaluate(2));
+		File f = new File("/Users/tamammakki/eclipse-workspace/b07 
+lab 1/src/polex");
+		Polynomial test = new Polynomial(f);
+		System.out.println(test.evaluate(1));
+		p1.saveToFile("/Users/tamammakki/eclipse-workspace/b07 lab 
+1/src/test");
 		}
 	} 
+
--- Polynomial.java
+++ Polynomial.java
@@ -1,67 +1,179 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Polynomial {
-	double[] co;
+	double[] coeffs;
+	int[] expos;
 	
 	public Polynomial() {
-		co = new double[1];
-		co[0] = 0;
+		coeffs = new double[1];
+		coeffs[0] = 0;
+		expos = new int[1];
+		expos[0] = 0;
 	}
-	public Polynomial(double[] cos) {
+	public Polynomial(double[] cos, int[] exp) {
 		int len = cos.length;
-		co = new double[len];
+		coeffs = new double[len];
+		expos = new int[len];
 		for(int i = 0; i < len; i++) {
-			co[i] = cos[i];
+			coeffs[i] = cos[i];
+			expos[i] = exp[i];
 		}
 	}
+	
+	public Polynomial(File polFile) {
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new 
+FileReader(polFile));
+			String line = reader.readLine();
+			int length = line.length();
+			double [] cos = new double[length];
+			int [] exp = new int[length];
+			boolean isNeg = false, isExp = false;
+			int i = 0, j = 0;
+			for(char s: line.toCharArray()) {
+				if(isExp) {//if last character was x
+					exp[j] = 
+Integer.parseInt(Character.toString(s));
+					j++;
+					isExp = false;
+				}
+				else if(s == 'x') {
+					isExp = true;
+				}
+				else if(s == '-') {
+					isNeg = true;
+				}
+				else if(s == '+')
+				{
+					continue;
+				}
+				else {
+					double num = 
+Double.parseDouble(Character.toString(s));
+					if(isNeg) {
+						num = -1*num;
+						isNeg = false;
+					}
+					
+					cos[i] = num;
+					i++;
+				}
+			}
+			coeffs = new double[i];
+			expos  = new int[j + 1];
+			for(int k = 0; k < i; k++) {
+				coeffs[k] = cos[k];
+			}
+			expos[0] = 0;
+			for(int k = 0; k < j; k++) {
+				expos[k + 1] = exp[k];
+			}
+			reader.close();
+		}
+		
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
 	public Polynomial add(Polynomial poly) {
 		int shorter_len;
 		int longer_len;
-		if(this.co.length > poly.co.length) {
-			longer_len = co.length;
-			shorter_len = poly.co.length;
+		if(this.coeffs.length > poly.coeffs.length) {
+			longer_len = coeffs.length;
+			shorter_len = poly.coeffs.length;
 		}
 		else {
-			longer_len = poly.co.length;
-			shorter_len = co.length;
+			longer_len = poly.coeffs.length;
+			shorter_len = coeffs.length;
 		}
-		double[] ans  =new double[longer_len];
+		double[] coAns = new double[longer_len];
+		int[] expAns = new int[longer_len];
 		int i = 0;
-		while(i < shorter_len) {
-			ans[i] = poly.co[i] + co[i];
+		while(i < shorter_len && poly.expos[i] == expos[i]) {
+			coAns[i] = poly.coeffs[i] + coeffs[i];
+			expAns[i] = poly.expos[i];
 			i++;
 		}
-		if(i == poly.co.length) {
+		if(i == poly.coeffs.length) {
 			while(i < longer_len) {
-				ans[i] = co[i];
+				coAns[i] = coeffs[i];
+				expAns[i] = expos[i];
 				i++;
 			}
 		}
 		else {
 			while(i < longer_len) {
-				ans[i] = poly.co[i];
+				coAns[i] = poly.coeffs[i];
+				expAns[i] = poly.expos[i];
 				i++;
 			}
 		}
-		Polynomial answer = new Polynomial(ans);
+		Polynomial answer = new Polynomial(coAns, expAns);
 		return answer;
 	}
+	
 	public double evaluate(double x) {
 		double xpow;
 		double solution = 0.0;
-		solution += co[0];
-		for(int i = 1; i < co.length; i++) {
-			xpow = Math.pow(x, i);
-			solution += co[i] * xpow;
+		for(int i = 0; i < coeffs.length; i++) {
+			xpow = Math.pow(x, expos[i]);
+			solution += coeffs[i] * xpow;
 		}
 		return solution;
 	}
+	
 	public boolean hasRoot(double val) {
-		boolean check = true;
-		if(evaluate(val) == 0) {
-			return check;
+		return evaluate(val) == 0;
+	}
+	
+	public Polynomial multiply(Polynomial pol) {
+		int shorter;
+		int longer;
+		if(this.coeffs.length > pol.coeffs.length) {
+			longer = this.coeffs.length;
+			shorter = pol.coeffs.length;
 		}
-		check = false;
-		return check;
+		else {
+			longer = pol.coeffs.length;
+			shorter = this.coeffs.length;
+		}
+		double[] coAns = new double[longer * shorter];
+		int[] expAns = new int[longer * shorter];
+		int k = 0;
+		for(int i = 0; i < longer; i++) {
+			for(int j = 0; j < shorter; j++) {
+				coAns[k] = pol.coeffs[i] * coeffs[j];
+				expAns[k] = pol.expos[i] + expos[j];
+				k++;
+			}
+		}
+		Polynomial soln = new Polynomial(coAns, expAns);
+		return soln;
+	}
+	
+	public void saveToFile(String fileName) throws IOException {
+		FileWriter writer = new FileWriter(fileName);
+		for(int i = 0; i < coeffs.length; i++)
+		{
+			if(i > 0 && coeffs[i] > 0)
+				writer.write("+");
+			writer.write((int)coeffs[i] + "");
+			if(i > 0) {
+				writer.write("x");
+				writer.write(expos[i] + "");
+			}
+		}
+		writer.write("\n");
+		writer.close();
 	}
 
 }
+
