094157710c7cc0fc70b196a9c1ad44ded9b95f3a
Tamam Makki <tamammakki@tamams-mbp.home>
2023-09-25 10:46:58 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -2,9 +2,9 @@ public class Driver {
     public static void main(String [] args) {
         Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
+        double [] c1 = {2,0,0,3};
         Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
+        double [] c2 = {-16,0,2,4,5};
         Polynomial p2 = new Polynomial(c2);
         Polynomial s = p1.add(p2);
         System.out.println("s(0.1) = " + s.evaluate(0.1));
