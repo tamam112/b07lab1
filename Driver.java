191de73878821d05352f8324fb5ec729c65bb63e
Tamam Makki <tamammakki@tamams-mbp.home>
2023-09-25 22:47:18 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,11 +1,13 @@
 public class Driver {
     public static void main(String [] args) {
         Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
-        double [] c1 = {2,0,0,3};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {-16,0,2,4,5};
-        Polynomial p2 = new Polynomial(c2);
+        System.out.println(p.evaluate(3)); //should evaulate to 0.0
+        double [] c1 = {6,0,0,5};
+        int [] e1 = {0,1,2,3};
+        Polynomial p1 = new Polynomial(c1, e1);
+        double [] c2 = {0,-2,0,0,-9};
+        int [] e2 = {0,1,2,3,4};
+        Polynomial p2 = new Polynomial(c2, e2);
         Polynomial s = p1.add(p2);
         System.out.println("s(0.1) = " + s.evaluate(0.1));
         if(s.hasRoot(1))
--- Polynomial.java
+++ Polynomial.java
@@ -3,28 +3,47 @@ import java.lang.Math;
 public class Polynomial {
 
     double[] coeffs;
+    int[] expons;
 
     public Polynomial(){
         coeffs = new double[1];
+        expons = new int[1];
         coeffs[0] = 0;
+        expons[0] = 0;
     }
 
-    public Polynomial(double[] cos){
+    public Polynomial(double[] cos, int[] exps){
         coeffs = new double[cos.length];
+        expons = new int[exps.length];
         for (int i = 0; i < cos.length; i++){
             coeffs[i] = cos[i];
+            expons[i] = exps[i];
         }
     }
+    //resume here, step b) changing exisitng methods according to extra array
 
     public Polynomial add(Polynomial poly){
         int longer = Math.max(coeffs.length, poly.coeffs.length);
         int shorter = Math.min(coeffs.length, poly.coeffs.length);
-        double[] sum = new double[longer];
+        double[] sumCoeffs = new double[longer];
+        int[] sumExpons = new int[longer];
         int i;
 
-        for (i = 0; i < shorter; i++){
-            sum[i] = coeffs[i] + poly.coeffs[i];
+        for (i = 0; i < shorter && expons[i] == poly.expons[i]; i++){
+            sumCoeffs[i] = coeffs[i] + poly.coeffs[i];
+            sumExpons[i] = expons[i];
         }
+        for(int j = i; j < sumCoeffs.length; j++){//is this the same as the commented out code below?
+            if (coeffs.length > poly.coeffs.length){
+                sumCoeffs[j] = coeffs[j];
+                sumExpons[j] = expons[j];
+            }
+            else{
+                sumCoeffs[j] = poly.coeffs[j];
+                sumExpons[j] = poly.expons[j];
+            }
+        }
+        /**
         if (coeffs.length > poly.coeffs.length) {
             for (int j = i; j < sum.length; j++) {
                 sum[j] = coeffs[j];
@@ -35,14 +54,15 @@ public class Polynomial {
                 sum[j] = poly.coeffs[j];
             }
         }
-        return new Polynomial(sum);
+         */
+        return new Polynomial(sumCoeffs, sumExpons);
     }
 
     public double evaluate(double x){
         //need to multiply each element in the array based on its exponent
-        double eval = coeffs[0];
-        for(int i = 1; i < coeffs.length; i++){
-            eval += coeffs[i]*Math.pow(x, i);
+        double eval = 0.0;
+        for(int i = 0; i < coeffs.length; i++){
+            eval += coeffs[i]*Math.pow(x, expons[i]);
         }
         return eval;
     }
